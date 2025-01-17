19fe46c8ac75477b47775c05cd20b35e2607dd16
Talib <abutalib715@gmail.com>
2025-01-17 06:33:58 +0600

    testing with junit 5 annotation

--- src/test/java/com/talib/services/CalculatorServiceJUnit5Test.java
+++ src/test/java/com/talib/services/CalculatorServiceJUnit5Test.java
@@ -1,25 +1,48 @@
 package com.talib.services;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.Date;
 
 public class CalculatorServiceJUnit5Test {
 
+    @BeforeAll
+    public static void init() {
+        System.out.println("Before start testing");
+        System.out.println("Start testing: " + new Date());
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("before each test case");
+    }
+
     @Test
+    @DisplayName("Custom Test")
     public void addTwoNumbersTest() {
         int actualResult = CalculatorService.addTwoNumbers(12, 45);
         int expectedResult = 57;
-
         Assertions.assertEquals(expectedResult, actualResult);
     }
 
 
     @Test
-    public void sumAnyNumbersTest() throws InterruptedException {
-
+    @Disabled
+    public void sumAnyNumbersTest() {
         int actualResult = CalculatorService.sumAnyNumbers(4, 6, 8, 2);
         int expectedResult = 20;
         Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @AfterAll
+    public static void cleanUp() {
+        System.out.println("After completing all test");
+        System.out.println("Testing done: " + new Date());
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("after each test case");
     }
 
 }
