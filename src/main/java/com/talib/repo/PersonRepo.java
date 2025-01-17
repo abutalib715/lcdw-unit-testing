5222843cf798824171d5b6614fea42d71f4f34b2
Talib <abutalib715@gmail.com>
2025-01-17 08:56:25 +0600

    person resource updated

--- src/main/java/com/talib/repo/PersonRepo.java
+++ src/main/java/com/talib/repo/PersonRepo.java
@@ -5,9 +5,9 @@ import com.talib.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface PersonRepo extends JpaRepository<Person, Long> {
+public interface PersonRepo extends JpaRepository<Person, Integer> {
 
-    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN TRUE ELSE FALSE END FROM Person p WHERE p.id=?")
+    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN TRUE ELSE FALSE END FROM Person p WHERE p.id=?1")
     Boolean isPersonExistById(Integer id);
 
 }
